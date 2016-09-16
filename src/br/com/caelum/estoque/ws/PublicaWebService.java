df29e82b1308f02b484d306e70448f8adc8dc795
saulo <saulocn@gmail.com>
2016-09-16 16:26:47 -0300

    - Import inicial;

---
+++ src/br/com/caelum/estoque/ws/PublicaWebService.java
@@ -0,0 +1,14 @@
+package br.com.caelum.estoque.ws;
+
+import javax.xml.ws.Endpoint;
+
+public class PublicaWebService {
+
+	public static void main(String[] args) {
+		EstoqueWs service = new EstoqueWs();
+		String url = "http://localhost:8087/estoquews";
+
+		Endpoint.publish(url, service);
+	}
+
+}
